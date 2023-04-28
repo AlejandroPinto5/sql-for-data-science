c45df27aa180c7bd6d5ce11fed7b5e11c5149ee4
Alejandro Pinto <43621473+AlejandroPinto5@users.noreply.github.com>
2023-04-28 17:35:54 -0500

    Update and rename datacamp.sql to basics.sql

---
+++ SQL-course-notes/basics.sql
@@ -0,0 +1,27 @@
+--BASIC SQL WHAT YOU'D LIKE TO DO WITH THE DATA
+
+-- Intro to SQL:
+--          SQL queries can be run successfully whether characters are written in upper- or lower-case. In other words, SQL queries are not case-sensitive. 
+--          It is common and best practice to capitalize all SQL commands, like SELECT and FROM, and keep everything else in your query lower case.
+--          It is common to use underscores and avoid spaces in column names.
+
+   -- Advantange: Many people can use at once
+   --          Can be secured with encryption
+   --          More Storage
+   --          An entity relationship diagram (ERD) is a common way to view data in a database
+   --          Consistent column types are one of the main reasons working with databases is fast
+   
+-- Tables:
+--          records(rows) and fields(columns)
+--          identifier is an unique value that identify a record
+
+--Commands:
+--          CREATE TABLE is a statement that creates a new table in a database.
+--          DROP TABLE is a statement that removes a table in a database.
+
+--          SELECT allows you to read data and display it. This is called a query. Commonly used. It indicates which column(s) you want to be given the data for.
+--          FROM specifies from which table(s) you want to select the columns. Notice the columns need to exist in this table.
+
+--          VARCHAR data type is very commonly used for storing strings.
+--          NUMERIC data type is commonly used for mathematical operations.
+--          INT data type is commonly used for storing numerical values.
--- SQL-course-notes/datacamp.sql
+++
@@ -1,5 +0,0 @@
--- Intro to SQL:
-
--- Advantange: Many people can use at once
-   --         Can be secured with encryption
-     --       More Storage
