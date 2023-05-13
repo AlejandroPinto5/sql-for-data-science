3fd2659bb86302fa74942edb998ab36357bcfdc9
Alejandro Pinto <43621473+AlejandroPinto5@users.noreply.github.com>
2023-05-13 10:31:24 -0500

    Update aggregations.sql

--- SQL-course-notes/aggregations.sql
+++ SQL-course-notes/aggregations.sql
@@ -8,8 +8,12 @@
     --NULL are a datatype that specifies where no data exists in SQL. = NULL doesn't work, NULL is not a values.
     --GROUP BY allows creating segments that will agregate independent from one another. It allows to takesum of data limitedto each account rather than across the entire dataset. The GROUP BY always goes between WHERE and ORDER BY.
     --DISTINCT is always used in SELECT statements, and it provides the unique rows for all columns written in the SELECT statement. 
+    --HAVING is the “clean” way to filter a query that has been aggregated, but this is also commonly done using a subquery. Any time you want to perform a WHERE on an element of your query that was created by an aggregate, you need to use HAVING instead.
+    --DATE_TRUNC allows you to truncate your date to a particular part of your date-time column. Common trunctions are day, month, and year. 
+    --DATE_PART can be useful for pulling a specific portion of a date, but notice pulling month or day of the week (dow) means that you are no longer keeping the years in order.
+    --CASE WHEN ELSE END statement always goes in the SELECT clause.
     
---Exmaples: Without []
+--Examples: Without []
     SELECT [column or columns or *] FROM [table] WHERE [column or columns or *] IS NULL;
     SELECT [column or columns or *] FROM [table] WHERE [column or columns or *] IS NOT NULL;
     SELECT COUNT([column or columns or *]) FROM [table] WHERE [conditional];
@@ -17,4 +21,7 @@
     SELECT MIN([column or columns or *]) AS [alias], MAX([column or columns or *]) AS [alias], ... FROM [table];
     SELECT AVG([column or columns or *]) AS [alias], MAX([column or columns or *]) AS [alias], ... FROM [table];
     SELECT [column or columns or *], SUM([column or columns or *]) AS [alias], ... FROM [table] GROUP BY [column or columns]; 
-    SELECT DISTINCT [COLUMN OR COLUMNS OR *] FROM table;
+    SELECT DISTINCT [column or columns or *] FROM table;
+    SELECT [column or columns or *], SUM([column or columns *]) FROM [table] GROUP BY [column] HAVING SUM([column or columns or *]) [condicional]
+    SELECT DATE_TRUNC('day', [column datetime data type]) AS [alias], ... FROM [table] GROUP BY DATE_TRUNC('day, [column datetime data type]) ORDER BY DATE_TRUNC('day, [column datetime data type])
+    select [column or oclumns or *] CASE [conditional or conditionals] FROM [table]
