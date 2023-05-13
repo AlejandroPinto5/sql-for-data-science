9abeae008341c3221c9e481ef94d2ec6a65c97d7
Alejandro Pinto <43621473+AlejandroPinto5@users.noreply.github.com>
2023-05-12 23:05:33 -0500

    Update aggregations.sql

--- SQL-course-notes/aggregations.sql
+++ SQL-course-notes/aggregations.sql
@@ -7,6 +7,7 @@
     --AVG calculates the average of all values in a particular column. This aggregate function again ignores the NULL values in both the numerator and the denominator.
     --NULL are a datatype that specifies where no data exists in SQL. = NULL doesn't work, NULL is not a values.
     --GROUP BY allows creating segments that will agregate independent from one another. It allows to takesum of data limitedto each account rather than across the entire dataset. The GROUP BY always goes between WHERE and ORDER BY.
+    --DISTINCT is always used in SELECT statements, and it provides the unique rows for all columns written in the SELECT statement. 
     
 --Exmaples: Without []
     SELECT [column or columns or *] FROM [table] WHERE [column or columns or *] IS NULL;
@@ -15,4 +16,5 @@
     SELECT SUM([column or columns or *]) AS [alias], SUM([column or columns or *]) AS [alias], ... FROM [table];
     SELECT MIN([column or columns or *]) AS [alias], MAX([column or columns or *]) AS [alias], ... FROM [table];
     SELECT AVG([column or columns or *]) AS [alias], MAX([column or columns or *]) AS [alias], ... FROM [table];
-    SELECT [column or columns or *], SUM([column or columns or *]) AS [alias], ... FROM [table] GROUP BY [first column in SELECT]; 
+    SELECT [column or columns or *], SUM([column or columns or *]) AS [alias], ... FROM [table] GROUP BY [column or columns]; 
+    SELECT DISTINCT [COLUMN OR COLUMNS OR *] FROM table;
