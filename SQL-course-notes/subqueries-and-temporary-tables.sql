d1dc919e34fcd364811b330add8bd16581af60b3
Alejandro Pinto <43621473+AlejandroPinto5@users.noreply.github.com>
2023-05-13 07:51:13 -0500

    Create subqueries-and-temporary-tables.sql

---
+++ SQL-course-notes/subqueries-and-temporary-tables.sql
@@ -0,0 +1,4 @@
+--SUBQUIERIES SQL
+
+-- Both subqueries and table expressions are methods for being able to write a query that creates a table, and then write a query that interacts with this newly created table.
+-- Subqueries allow you to answer more complex questions than you can with a single database table. 
