e40e1535aaa860695f0e07c64ced064f80069d36
Alejandro Pinto <43621473+AlejandroPinto5@users.noreply.github.com>
2023-05-13 18:25:19 -0500

    Finished aggregations.sql

--- SQL-course-notes/aggregations.sql
+++ SQL-course-notes/aggregations.sql
@@ -24,5 +24,5 @@
     SELECT DISTINCT [column or columns or *] FROM table;
     SELECT [column or columns or *], SUM([column or columns *]) FROM [table] GROUP BY [column] HAVING SUM([column or columns or *]) [condicional]
     SELECT DATE_TRUNC('day', [column datetime data type]) AS [alias], ... FROM [table] GROUP BY DATE_TRUNC('day, [column datetime data type]) ORDER BY DATE_TRUNC('day, [column datetime data type])
-    SELECT [column or oclumns or *] CASE [conditional or conditionals] END FROM [table]
+    SELECT [column or oclumns or *] CASE [conditional or conditionals] END AS [alias] FROM [table]
 -----------------------------------------------------------------------------------------------
