cb5953ce9aeafcb43599f1f83c4342a2b11e132f
Alejandro Pinto <43621473+AlejandroPinto5@users.noreply.github.com>
2023-05-14 09:31:34 -0500

    Update subqueries-and-temporary-tables.sql

--- SQL-course-notes/subqueries-and-temporary-tables.sql
+++ SQL-course-notes/subqueries-and-temporary-tables.sql
@@ -1,4 +1,7 @@
---SUBQUIERIES SQL
+--SUBQUIERIES (INNER QUERIES OR NESTED QUERIES) SQL
 
 -- Both subqueries and table expressions are methods for being able to write a query that creates a table, and then write a query that interacts with this newly created table.
 -- Subqueries allow you to answer more complex questions than you can with a single database table. 
+
+  --Examples: Without []
+    SELECT [column or columns or * or aggregations] FROM ([query]) [alias];
